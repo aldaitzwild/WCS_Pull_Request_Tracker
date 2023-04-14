7e4dd6198f756a8e9dc599fe90e4e83dad54fff4
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2023-04-14 14:32:24 +0200

    wooot repository, entity controller for create a project

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProjectRepository;
+
+#[Route('/project', name: 'project_')]
+class ProjectController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(ProjectRepository $projectRepository): Response
+    {
+        $projects = $projectRepository->findAll();
+
+        return $this->render('project/index.html.twig', [
+            'projects' => $projects,
+        ]);
+    }
+}
---
+++ src/Entity/Project.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $github_link = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getGithubLink(): ?string
+    {
+        return $this->github_link;
+    }
+
+    public function setGithubLink(string $github_link): self
+    {
+        $this->github_link = $github_link;
+
+        return $this;
+    }
+}
