fbb5bfed9ec1c3c9fbc1c858da63bddc7b00828f
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2023-05-05 10:51:05 +0200

    remove useless use

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,13 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Project;
 use App\Form\ProjectType;
-use App\Form\SearchContributorType;
 use App\Repository\ContributorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProjectRepository;
 
