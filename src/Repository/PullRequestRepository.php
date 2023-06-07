9df37f8647ffbd5be2293c30f90c3506d314248f
Axx972 <123100991+Axx972@users.noreply.github.com>
2023-06-07 09:17:05 +0200

    creating the entity pull_request

---
+++ src/Repository/PullRequestRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\PullRequest;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<PullRequest>
+ *
+ * @method PullRequest|null find($id, $lockMode = null, $lockVersion = null)
+ * @method PullRequest|null findOneBy(array $criteria, array $orderBy = null)
+ * @method PullRequest[]    findAll()
+ * @method PullRequest[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PullRequestRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, PullRequest::class);
+    }
+
+    public function save(PullRequest $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(PullRequest $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return PullRequest[] Returns an array of PullRequest objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?PullRequest
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
