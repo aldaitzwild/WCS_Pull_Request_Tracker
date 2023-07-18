9e19c6b24255f1add652780c61007535327c4cd6
zakaria6907 <bourmelzakaria@outlook.fr>
2023-07-18 12:24:50 +0200

    remove symfony-ux live component

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
-    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
